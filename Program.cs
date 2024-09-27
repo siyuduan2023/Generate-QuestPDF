a67ff85e1843c05a70c6e7fb248c4a02d610982b
Siyu <siyusiyuduan@hotmail.com>
2024-09-27 23:31:12 +0100

    Previewer installed successfully

--- Program.cs
+++ Program.cs
@@ -13,21 +13,21 @@ Document.Create(container =>
         page.Margin(2, Unit.Centimetre);
         page.PageColor(Colors.White);
         page.DefaultTextStyle(x => x.FontSize(20));
-        
+
         page.Header()
             .Text("Hello PDF!")
             .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
-        
+
         page.Content()
             .PaddingVertical(1, Unit.Centimetre)
             .Column(x =>
             {
                 x.Spacing(20);
-                
+
                 x.Item().Text(Placeholders.LoremIpsum());
                 x.Item().Image(Placeholders.Image(200, 100));
             });
-        
+
         page.Footer()
             .AlignCenter()
             .Text(x =>
